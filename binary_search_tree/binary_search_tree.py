d611d102673dfc852b76b98b8d3837522d3ddd1a
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-02 14:37:06 -0400

    BST started, first 2 methods complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,42 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+        else:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        if self.value == target:
+            return True
 
-  def for_each(self, cb):
-    pass
+        if target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+
+    def get_max(self):
+        pass
+
+    def for_each(self, cb):
+        pass
+
+    def __str__(self):
+        return str(self.value)
