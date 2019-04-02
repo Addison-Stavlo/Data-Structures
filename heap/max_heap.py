5333ffb2fdc7a449d5dc3056cb0063e8f331b205
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-02 18:16:35 -0400

    heap insert complete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -4,10 +4,22 @@ class Heap:
 
     def insert(self, value):
         # insert at end of heap
-        # compare with parent
-        # swap if larger than parent
-        # continue compare/swap until parent is larger
-        pass
+        new_index = len(self.storage)
+        self.storage.append(value)
+        found_spot = False
+
+        while not found_spot:
+            # compare with parent
+            parent_index = (new_index - 1) // 2
+            if parent_index < 0:
+                parent_index = 0
+            # swap if larger than parent
+            if self.storage[new_index] > self.storage[parent_index]:
+                self.storage[new_index], self.storage[parent_index] = self.storage[parent_index], self.storage[new_index]
+                new_index = parent_index
+            # continue compare/swap until parent is larger
+            else:
+                found_spot = True
 
     def delete(self):
         # remove element from front of list
@@ -28,3 +40,13 @@ class Heap:
 
     def _sift_down(self, index):
         pass
+
+
+heap = Heap()
+heap.insert(5)
+heap.insert(2)
+heap.insert(7)
+heap.insert(12)
+heap.insert(1)
+heap.insert(22)
+print(heap.storage)
