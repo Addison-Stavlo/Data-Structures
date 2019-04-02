bbd6f3964e866347d0e8b9a7a19772b9b74390ca
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-02 17:51:50 -0400

    heaps started, pseudo  code for first 2 methods added

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,30 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # insert at end of heap
+        # compare with parent
+        # swap if larger than parent
+        # continue compare/swap until parent is larger
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        # remove element from front of list
+        # copy last value to beginning, remove last index off list
+        # compare with largest child
+        # swap if smaller than largest child
+        # repeat comparisons until child is smaller
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
